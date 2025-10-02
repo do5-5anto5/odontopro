3781700c3ea0b62f8c96ce4a6ed27e40b706c1ac
Jonathan <dos.santos.2k23@gmail.com>
2025-10-02 18:01:10 -0300

    feat: implement the mapping of the user object obtained in the session

---
+++ types/next-auth.d.ts
@@ -0,0 +1,22 @@
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: User & DefaultSession["user"];
+  }
+}
+
+interface User {
+  ind: string;
+  name: string;
+  email: string;
+  emailVerified?: null | boolean | string;
+  image?: string;
+  stripe_customer_id?: string;
+  times: [];
+  adress: ?string;
+  phone?: string;
+  status: boolean;
+  createdAt: string,
+  updatedAt: string
+}
