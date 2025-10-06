34fb0492ad5404cc68956a17dd9f912efe77fe38
Jonathan <dos.santos.2k23@gmail.com>
2025-10-06 19:22:32 -0300

    feat:(1.1): preparing useProfileForm to receive user data retrieved - create an interface to map it in a type with nullable attributes - expect the type attributes as parameters to fill the form

--- src/app/(panel)/dashboard/profile/_components/profile-form.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile-form.tsx
@@ -2,6 +2,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+interface UseProfileFormProps {
+  name: string | null;
+  adress: string | null;
+  phone: string | null;
+  status:  boolean;
+  timezone: string | null;
+}
+
 const profileSchema = z.object({
   name: z.string().min(1, { message: "Campo nome é necessário" }),
   adress: z.string().optional(),
@@ -12,16 +20,21 @@ const profileSchema = z.object({
 
 export type ProfileFormData = z.infer<typeof profileSchema>;
 
-export function useProfileForm() {
-    return useForm<ProfileFormData>(
-        {resolver: zodResolver(profileSchema),
-            defaultValues:{
-                name: "",
-                adress: "",
-                phone: "",
-                status: "",
-                timezone: "",
-            }
-        }
-    )
+export function useProfileForm({
+  name,
+  adress,
+  phone,
+  status,
+  timezone,
+}: UseProfileFormProps) {
+  return useForm<ProfileFormData>({
+    resolver: zodResolver(profileSchema),
+    defaultValues: {
+      name: name || "",
+      adress: adress || "",
+      phone: phone || "",
+      status: status ? "active" : "inactive",
+      timezone: timezone || "",
+    },
+  });
 }
