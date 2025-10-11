b74192f45afb934f8a51a2d453c046f211bb81f9
Jonathan <dos.santos.2k23@gmail.com>
2025-10-11 07:57:59 -0300

    feat: first approach of this UI changes at services page

--- src/app/(panel)/dashboard/services/page.tsx
+++ src/app/(panel)/dashboard/services/page.tsx
@@ -1,7 +1,15 @@
-export default function Services() {
+import getSession from '@/lib/getSession'
+import { redirect } from 'next/navigation'
+import { ServicesContent } from './_components/services-content'
+
+export default async function Services() {
+  const session = await getSession()
+
+  if (!session || !session.user) redirect('/')
+
   return (
     <section>
-      <h1>Serviços</h1>
+      <ServicesContent userId={session.user?.id} />
     </section>
-  );
+  )
 }
