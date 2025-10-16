9965d0653dfa6e9047e50d3ed3b167d17db9eab2
Jonathan <dos.santos.2k23@gmail.com>
2025-10-16 18:58:07 -0300

    feat: implement logic to endpoint /api/schedule/get-appointments: - retrieves all appointments for a given user on a given date and returns object or error if no user or date is provided

--- src/app/api/schedule/get-appointments/route.ts
+++ src/app/api/schedule/get-appointments/route.ts
@@ -1,8 +1,53 @@
-import prisma from '@/generated/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
+/**
+ * GET /api/schedule/get-appointments
+ *
+ * Retrieves all appointments for a given user on a given date.
+ *
+ * @param {NextRequest} request - The request object.
+ *
+ * @returns {NextResponse} - The response object.
+ *
+ * @throws {NextResponse} - If no user or date is provided, returns an error response with status 400.
+ */
 export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl
+
+  const userId = searchParams.get('userId')
+  const dateParam = searchParams.get('date')
+
+  if (!userId || userId === 'null' || !dateParam || dateParam === 'null') {
+    return NextResponse.json(
+      {
+        error: 'Nenhum agendamento encontrado',
+      },
+      {
+        status: 400,
+      }
+    )
+  }
+
+  try {
+    const [year, month, day] = dateParam.split('-').map(Number)
+    const startDate = new Date(year, month - 1, day, 0, 0, 0)
+    const endDate = new Date(year, month - 1, day, 23, 59, 59, 999)
+
+    console.log('start date', startDate)
+    console.log('end date', endDate)
+
     return NextResponse.json({
-      ok: true  
+        ok: true
     })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      {
+        error: 'Nenhum agendamento encontrado',
+      },
+      {
+        status: 400,
+      }
+    )
+  }
 }
