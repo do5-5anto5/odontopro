2028be6a60a83402c443fe86243a5482f88c2240
Jonathan <dos.santos.2k23@gmail.com>
2025-10-16 22:22:31 -0300

    feat: enhance get-appointments route logic: - filters times at given date - returns an array of blocked times for found appointments

--- src/app/api/schedule/get-appointments/route.ts
+++ src/app/api/schedule/get-appointments/route.ts
@@ -1,9 +1,13 @@
+import { User } from './../../../../generated/prisma/index.d'
+import prisma from '@/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
 /**
  * GET /api/schedule/get-appointments
  *
  * Retrieves all appointments for a given user on a given date.
+ * 
+ * @description Returns blocked hours for appointments
  *
  * @param {NextRequest} request - The request object.
  *
@@ -33,12 +37,56 @@ export async function GET(request: NextRequest) {
     const startDate = new Date(year, month - 1, day, 0, 0, 0)
     const endDate = new Date(year, month - 1, day, 23, 59, 59, 999)
 
-    console.log('start date', startDate)
-    console.log('end date', endDate)
-
-    return NextResponse.json({
-        ok: true
+    const user = await prisma.user.findFirst({
+      where: {
+        id: userId,
+      },
     })
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: 'Não há clínica com Id correspondente',
+        },
+        {
+          status: 400,
+        }
+      )
+    }
+
+    const appointments = await prisma.appointment.findMany({
+      where: {
+        userId: userId,
+        appointmentDate: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+      include: {
+        service: true,
+      },
+    })
+
+    const blockedSlots = new Set<string>()
+
+    for (const apt of appointments) {
+      const requiredSlots = Math.ceil(apt.service.duration / 30)
+      const startIndex = user.times.indexOf(apt.time)
+
+      if (startIndex !== -1) {
+        for (let i = 0; i < requiredSlots; i++) {
+          const blockedSlot = user.times[startIndex + i]
+          if (blockedSlot) {
+            blockedSlots.add(blockedSlot)
+          }
+        }
+      }
+    }
+
+    const blockedTimes = Array.from(blockedSlots)
+    console.log('BLOCKED TIMES', blockedTimes)
+
+    return NextResponse.json(blockedTimes)
   } catch (error) {
     console.log(error)
     return NextResponse.json(
