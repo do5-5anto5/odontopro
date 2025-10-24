c262b32469141af8ae88273e2927c0b61840c9f0
Jonathan <dos.santos.2k23@gmail.com>
2025-10-23 21:12:18 -0300

    feat: fetch appointments data per date

--- src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
+++ src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
@@ -2,13 +2,10 @@
 
 import { useSearchParams } from 'next/navigation'
 
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { useQuery } from '@tanstack/react-query'
+import { format } from 'date-fns'
 
 interface AppointmentsListProps {
   times: string[]
@@ -23,8 +20,31 @@ interface AppointmentsListProps {
  */
 export function AppointmentsList({ times }: AppointmentsListProps) {
   const searchParams = useSearchParams()
-
   const date = searchParams.get('date')
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-appointments', date],
+    queryFn: async () => {
+      let activeDate = date
+
+      if (!activeDate) {
+        const today = format(new Date(), 'yyyy-MM-dd')
+        activeDate = today
+      }
+
+      const url = `${process.env.NEXT_PUBLIC_URL}/api/clinic/appointments?date=${activeDate}`
+
+      const response = await fetch(url)
+
+      const json = await response.json()
+
+      console.log(json)
+
+      if (!response.ok) return []
+
+      return json
+    },
+  })
 
   return (
     <div>
@@ -41,9 +61,12 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
           <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4">
             {times.map((slot) => {
               return (
-                <div key={slot} className='flex items-center py-2 border-t last:border-b'>
-                  <div className='w-16 text-sm font-semibold'>{slot}</div>
-                  <div className='flex-1 text-sm text-gray-500'>Disponível</div>
+                <div
+                  key={slot}
+                  className="flex items-center py-2 border-t last:border-b"
+                >
+                  <div className="w-16 text-sm font-semibold">{slot}</div>
+                  <div className="flex-1 text-sm text-gray-500">Disponível</div>
                 </div>
               )
             })}
