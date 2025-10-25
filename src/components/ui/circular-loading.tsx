8859aeed26579e29b57ea8ccaf65963851db9234
Jonathan <dos.santos.2k23@gmail.com>
2025-10-24 23:18:54 -0300

    feat: adjust CircularLoading size

--- src/components/ui/circular-loading.tsx
+++ src/components/ui/circular-loading.tsx
@@ -7,7 +7,7 @@ export default function CircularLoading({
     <div
       className={`w-5 h-5 border-2 border-${borderColor} border-t-transparent rounded-full animate-spin text-sm bg-emerald-500      aria-label="Carregando..."`}
     >
-      <div className="w-3 h-3 bg-white rounded-full"></div>
+      <div className="w-2 h-2 bg-white rounded-full"></div>
     </div>
   )
 }
