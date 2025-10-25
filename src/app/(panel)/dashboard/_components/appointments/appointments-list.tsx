a76ffe80658c56452bc63ccb2dfa5900c0e9cf69
Jonathan <dos.santos.2k23@gmail.com>
2025-10-24 23:18:23 -0300

    feat: improve AppointmentsList component - highlighting the time slots occupied by appointments from the available times

--- src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
+++ src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
@@ -6,10 +6,18 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { useQuery } from '@tanstack/react-query'
 import { format } from 'date-fns'
+import { Prisma } from '@/generated/prisma'
+import CircularLoading from '@/components/ui/circular-loading'
 
 interface AppointmentsListProps {
   times: string[]
 }
+
+type AppointmentWithService = Prisma.AppointmentGetPayload<{
+  include: {
+    service: true
+  }
+}>
 
 /**
  * Render a list of available time slots for a given date and clinic.
@@ -22,7 +30,7 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
   const searchParams = useSearchParams()
   const date = searchParams.get('date')
 
-  const { data, isLoading } = useQuery({
+  const { data: appointments, isLoading } = useQuery({
     queryKey: ['get-appointments', date],
     queryFn: async () => {
       let activeDate = date
@@ -38,13 +46,33 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
 
       const json = await response.json()
 
-      console.log(json)
+      console.log(activeDate, ' ', json)
 
       if (!response.ok) return []
 
-      return json
+      return json.appointments as AppointmentWithService[]
     },
   })
+
+  const occupantMap: Record<string, AppointmentWithService> = {}
+
+  if (appointments && appointments.length > 0) {
+    for (const appointment of appointments) {
+      const requiredSlots = Math.ceil(appointment.service.duration / 30)
+
+      const startIndex = times.indexOf(appointment.time)
+
+      if (startIndex !== -1) {
+        for (let i = 0; i < requiredSlots; i++) {
+          const slotIndex = startIndex + i
+
+          if (slotIndex < times.length) {
+            occupantMap[times[slotIndex]] = appointment
+          }
+        }
+      }
+    }
+  }
 
   return (
     <div>
@@ -59,17 +87,43 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
 
         <CardContent>
           <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4">
-            {times.map((slot) => {
-              return (
-                <div
-                  key={slot}
-                  className="flex items-center py-2 border-t last:border-b"
-                >
-                  <div className="w-16 text-sm font-semibold">{slot}</div>
-                  <div className="flex-1 text-sm text-gray-500">Disponível</div>
-                </div>
-              )
-            })}
+            {isLoading ? (
+              <div className='flex flex-row items-center'>
+                <p className="text-lg mr-2">Carregando agenda</p>
+                <CircularLoading borderColor="emerald-800" />
+              </div>
+            ) : (
+              times.map((slot) => {
+                const occupant = occupantMap[slot]
+
+                if (occupant) {
+                  return (
+                    <div
+                      key={slot}
+                      className="flex items-center py-2 border-t last:border-b"
+                    >
+                      <div className="w-16">{slot}</div>
+                      <div className="flex-1">
+                        <div className="font-semibold">{occupant.name}</div>
+                        <div className="text-sm text-gray-500">
+                          {occupant.phone}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                } else {
+                  return (
+                    <div
+                      key={slot}
+                      className="flex items-center py-2 border-t last:border-b"
+                    >
+                      <div className="w-16 text-sm font-semibold">{slot}</div>
+                      <div className="flex-1 text-sm">Disponível</div>
+                    </div>
+                  )
+                }
+              })
+            )}
           </ScrollArea>
         </CardContent>
       </Card>
