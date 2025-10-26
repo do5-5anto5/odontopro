8351019a163572a49dc30dec0deda64f95f1a7ad
Jonathan <dos.santos.2k23@gmail.com>
2025-10-26 15:03:48 -0300

    refactor: giving an optional border to CircularLoading component

--- src/components/ui/circular-loading.tsx
+++ src/components/ui/circular-loading.tsx
@@ -1,11 +1,14 @@
 export default function CircularLoading({
   borderColor,
+  noBorder = false
 }: {
-  borderColor: string
+  noBorder?: boolean
+  borderColor?: string
 }) {
+  const border = noBorder? '' : `border-2 border-${borderColor}`
   return (
     <div
-      className={`w-5 h-5 border-2 border-${borderColor} border-t-transparent rounded-full animate-spin text-sm bg-emerald-500      aria-label="Carregando..."`}
+      className={`w-5 h-5 ${border} rounded-full animate-spin text-sm bg-emerald-500      aria-label="Carregando..."`}
     >
       <div className="w-2 h-2 bg-white rounded-full"></div>
     </div>
