defd00efd4197fe13a5e962f70a2311b81d4d432
Jonathan <dos.santos.2k23@gmail.com>
2025-10-26 17:57:20 -0300

    feat: List professials clinics at public home page

--- src/app/(public)/_components/professionals.tsx
+++ src/app/(public)/_components/professionals.tsx
@@ -1,10 +1,17 @@
-import { Card, CardContent } from "@/components/ui/card";
-import Image from "next/image";
-import photoImg from "../../../../public/foto1.png";
-import Link from "next/link";
-import { ArrowRight } from "lucide-react";
+import { Card, CardContent } from '@/components/ui/card'
+import Image from 'next/image'
+import photoImg from '../../../../public/foto1.png'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
+import { User } from '@/generated/prisma/client'
 
-export function Professionals() {
+interface ProfessionalsProps {
+  professionals: User[]
+}
+
+export function Professionals({ professionals }: ProfessionalsProps) {
+  console.log('profs', professionals)
+
   return (
     <section className="bg-gray-50 py-16">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
@@ -13,44 +20,47 @@ export function Professionals() {
         </h2>
 
         <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
-          <Card className="overflow-hidden p-0">
-            <CardContent className="p-0">
-              <div>
-                <div className="relative h-48">
-                  <Image
-                    src={photoImg}
-                    alt="Imagem de perfil da clínica"
-                    fill
-                    className="object-cover"
-                  ></Image>
+          {professionals.map((clinic) => (
+            <Card className="overflow-hidden p-0 hover:shadow-2xl" key={clinic.id}>
+              <CardContent className="p-0">
+                <div>
+                  <div className="relative h-48">
+                    <Image
+                      src={clinic.image || photoImg}
+                      alt="Imagem de perfil da clínica"
+                      fill
+                      className="object-cover"
+                    ></Image>
+                  </div>
                 </div>
-              </div>
 
-              <div className="p-4 space-y-4">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <h3 className="font-semibold">Clínica Centro</h3>
-                    <p className="text-sm text-gray-500">
-                      Rua X, centro, BH - MG
-                    </p>
+                <div className="p-4 space-y-4">
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <h3 className="font-semibold">{clinic.name}</h3>
+                      <p className="text-sm text-gray-500">
+                        {clinic.adress ?? 'Endereço não informado'}
+                      </p>
+                    </div>
+
+                    <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
                   </div>
 
-                  <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
+                  <Link
+                    href={`/clinica/${clinic.id}`}
+                    className="w-full bg-emerald-500 hover:bg-emerald-400 text-white 
+                      flex items-center justify-center py-2 rounded-md text-sm md:text-base font-medium"
+                    target='_blank'
+                  >
+                    <ArrowRight />
+                    Agendar horário
+                  </Link>
                 </div>
-
-                <Link
-                  href="/clinica/123"
-                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white 
-                  flex items-center justify-center py-2 rounded-md text-sm md:text-base font-medium"
-                >
-                  <ArrowRight />
-                  Agendar horário
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
+              </CardContent>
+            </Card>
+          ))}
         </section>
       </div>
     </section>
-  );
+  )
 }
