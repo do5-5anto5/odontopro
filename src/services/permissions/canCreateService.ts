4216eb0418feebfeb03ff89c99488384d8216108
Jonathan <dos.santos.2k23@gmail.com>
2025-11-03 21:10:43 -0300

    feat: integrate checkSubscriptionExpired in canCreateService.ts

--- src/services/permissions/canCreateService.ts
+++ src/services/permissions/canCreateService.ts
@@ -5,16 +5,21 @@ import prisma from '@/lib/prisma'
 import { Session } from 'next-auth'
 import { getPlan } from './get-plan'
 import { PLANS } from '@/utils/plans'
+import { checkSubscriptionExpired } from './checkSubscriptionExpired'
+import { ResultPermissionProps } from './planPermissions'
+
 
 /**
- * Checks if the user can create a new service based on the active subscription
- * and the number of services they already have. 
+ * Checks if the user has permission to create a new service.
+ * If the user has an active subscription, it will check if the user has
+ * reached the maximum number of services allowed by the plan.
+ * If the user does not have an active subscription, it will check if the
+ * user's trial period has expired.
  */
-
 export async function canCreateSerivce(
   subscription: Subscription | null,
   session: Session
-) {
+): Promise<ResultPermissionProps> {
   try {
     const serviceCount = await prisma.service.count({
       where: {
@@ -35,7 +40,18 @@ export async function canCreateSerivce(
         plan: PLANS[subscription.plan],
       }
     }
+
+    const checkUserLimit = await checkSubscriptionExpired(session)
+
+    return checkUserLimit
   } catch (error) {
     console.log(error)
+
+    return {
+      hasPermission: false,
+      planId: 'EXPIRED',
+      expired: true,
+      plan: null,
+    }
   }
 }
