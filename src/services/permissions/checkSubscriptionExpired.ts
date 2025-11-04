550ed4ff54cd4048d322d70d0cf949a7ff58992e
Jonathan <dos.santos.2k23@gmail.com>
2025-11-03 21:03:40 -0300

    feat: add checkSubscriptionExpired.ts - Checks if the user's trial period has expired

---
+++ src/services/permissions/checkSubscriptionExpired.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import { addDays, isAfter } from 'date-fns'
+import { Session } from 'next-auth'
+import { ResultPermissionProps } from './planPermissions'
+
+const TRIAL_DAYS = 3
+
+/**
+ * Checks if the user's trial period has expired.
+ */
+export async function checkSubscriptionExpired(
+  session: Session
+): Promise<ResultPermissionProps> {
+  const trialEndDate = addDays(session.user.createdAt, TRIAL_DAYS)
+
+  if (isAfter(new Date(), trialEndDate)) {
+    return {
+      hasPermission: false,
+      planId: 'EXPIRED',
+      expired: true,
+      plan: null,
+    }
+  }
+
+  return {
+    hasPermission: true,
+    planId: 'TRIAL',
+    expired: false,
+    plan: null,
+  }
+}
