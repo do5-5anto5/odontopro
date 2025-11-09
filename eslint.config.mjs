1d5a2e331b0db3749494f1aaf5c35c8b97294c5e
Jonathan <dos.santos.2k23@gmail.com>
2025-11-09 16:21:54 -0300

    update: set up eslint.config.mjs to ignore generated files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+      '**/dist/**',
+      '**/*.config.js',
+      'next-env.d.ts',
+      'src/generated/**', // Adiciona isso
+      '**/generated/**',
     ],
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
